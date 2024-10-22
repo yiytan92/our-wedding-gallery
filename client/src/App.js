b74b9d4eed8386e9e5efa8dd36c140e1026c14f6
Yi Yang Tan <yiyang.tan@ufinity.com>
2024-10-22 12:23:46 +0800

    update app

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import LoadingBar from './component/LoadingBar';
 import PhotoUpload from './component/PhotoUpload';
 import PhotoGallery from './component/PhotoGallery';
 import logo from './images/surnames.jpg';
+import invitationCard from './images/invitation-card.jpg';
+
 
 function App({ bootstrapUrl }) {
   const [bootstrap, setBootstrap] = useState(undefined);
@@ -19,10 +21,13 @@ function App({ bootstrapUrl }) {
 
   return (
     <>
-      <div className="mt-10 mb-10 flex justify-center">
-        <img src={logo} alt="" width="100%" height="400px" />
+      <div className="flex justify-center h-[400px]">
+        <img src={logo} alt="" width="100%" />
       </div>
-      <div className="mt-10 mb-10 flex justify-center">
+      <div className="flex justify-center">
+        <img src={invitationCard} alt="" width="100%" />
+      </div>
+      <div className="flex justify-center">
         {bootstrap?._links?.request && (
           <PhotoUpload
             url={bootstrap._links.request.href}
