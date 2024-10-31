48c81b70a6704688c519f610bf83714dab72968c
Yi Yang Tan <yiyang.tan@ufinity.com>
2024-10-31 17:40:05 +0800

    update loading bar

--- client/src/component/LoadingBar.js
+++ client/src/component/LoadingBar.js
@@ -1,9 +1,9 @@
 function LoadingBar() {
   return (
     <div className="mt-10 mb-10 flex items-center justify-center space-x-2 animate-pulse">
-      <div className="w-8 h-8 bg-cadbury rounded-full"></div>
-      <div className="w-8 h-8 bg-cadbury rounded-full"></div>
-      <div className="w-8 h-8 bg-cadbury rounded-full"></div>
+      <div className="w-8 h-8 bg-red-800 rounded-full"></div>
+      <div className="w-8 h-8 bg-red-800 rounded-full"></div>
+      <div className="w-8 h-8 bg-red-800 rounded-full"></div>
     </div>
   );
 }
--- client/src/component/PhotoUpload.js
+++ client/src/component/PhotoUpload.js
@@ -112,7 +112,7 @@ function PhotoUpload({ url, maxPhotosPerRequest, onUpload }) {
     <>
       <button
         class="button"
-        // className="block text-white bg-cadbury hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        // className="block text-white bg-red-800 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button"
         onClick={() => setOpen(true)}
       >
